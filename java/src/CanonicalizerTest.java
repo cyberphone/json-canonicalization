cb7f6dc4c28b98a5cfa8dc86d3f5f77b048114c6
Anders Rundgren <anders.rundgren.net@gmail.com>
2018-03-14 10:57:48 +0100

    Added Unicode control characters to "weird.json"

--- java/src/CanonicalizerTest.java
+++ java/src/CanonicalizerTest.java
@@ -15,24 +15,23 @@ public class CanonicalizerTest {
         byte[] rawInput = ArrayUtil.readFile(inputDirectory + File.separator + fileName);
         byte[] expected = ArrayUtil.readFile(outputDirectory + File.separator + fileName);
         byte[] actual = JSONParser.parse(rawInput).serializeToBytes(JSONOutputFormats.CANONICALIZED);
-        if (ArrayUtil.compare(expected, actual)) {
-            StringBuilder utf8 = new StringBuilder("\nFile: ");
-            utf8.append(fileName).append("\n");
-            int byteCount = 0;
-            boolean next = false;
-            for (byte b : actual) {
-                if (byteCount++ % 32 == 0) {
-                    utf8.append('\n');
-                    next = false;
-                }
-                if (next) {
-                    utf8.append(" ");
-                }
-                next = true;
-                utf8.append(DebugFormatter.getHexString(new byte[]{b}));
-            }
-            System.out.println(utf8.append("\n").toString());
-        } else {
+		StringBuilder utf8InHex = new StringBuilder("\nFile: ");
+		utf8InHex.append(fileName).append("\n");
+		int byteCount = 0;
+		boolean next = false;
+		for (byte b : actual) {
+			if (byteCount++ % 32 == 0) {
+				utf8InHex.append('\n');
+				next = false;
+			}
+			if (next) {
+				utf8InHex.append(" ");
+			}
+			next = true;
+			utf8InHex.append(DebugFormatter.getHexString(new byte[]{b}));
+		}
+		System.out.println(utf8InHex.append("\n").toString());
+        if (!ArrayUtil.compare(expected, actual)) {
             throw new RuntimeException("Failed for file: " + fileName);
         }
     }
