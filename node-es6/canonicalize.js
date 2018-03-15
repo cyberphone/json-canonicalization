f328dcde637285ba5fef0d9e71ee760149b4c566
Anders Rundgren <anders.rundgren.net@gmail.com>
2018-03-15 15:23:12 +0100

    Major I-D update

--- node-es6/canonicalize.js
+++ node-es6/canonicalize.js
@@ -1,4 +1,4 @@
-// ES6 JSON Canonicalizer
+// ES6 based JSON canonicalizer
 'use strict';
 var canonicalize = function(object) {
 
@@ -11,29 +11,35 @@ var canonicalize = function(object) {
             if (Array.isArray(object)) {
                 buffer += '[';
                 let next = false;
+                // Array - Maintain element order
                 object.forEach((element) => {
                     if (next) {
                         buffer += ',';
                     }
                     next = true;
+                    // Recursive call
                     serialize(element);
                 });
                 buffer += ']';
             } else {
                 buffer += '{';
                 let next = false;
+                // Object - Sort properties before serializing
                 Object.keys(object).sort().forEach((property) => {
                     if (next) {
                         buffer += ',';
                     }
                     next = true;
+                    // Properties are just strings - Use ES6
                     buffer += JSON.stringify(property);
                     buffer += ':';
+                    // Recursive call
                     serialize(object[property]);
                 });
                 buffer += '}';
             }
         } else {
+            // Primitive data type - Use ES6
             buffer += JSON.stringify(object);
         }
     }
