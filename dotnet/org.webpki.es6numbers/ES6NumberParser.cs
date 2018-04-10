e2d67ce2891a2313a826e656f8c70b869e36080a
Anders Rundgren <anders.rundgren.net@gmail.com>
2018-04-10 04:53:37 +0200

    Temporary backup

--- dotnet/org.webpki.es6numbers/ES6NumberParser.cs
+++ dotnet/org.webpki.es6numbers/ES6NumberParser.cs
@@ -35,110 +35,159 @@ namespace Org.Webpki.Es6Numbers
 
         const ulong MSB_U64                 = 0x8000000000000000L;
         const ulong MANTISSA_ROUNDER        = 0x0000000000000100L;
-        const ulong SCALE_POINT             = 0xe000000000000000L;
+        const ulong SCALE_POINT_MASK        = 0xe000000000000000L;
         const ulong LARGEST_SAFE_MULTIPLIER = 0x1000000000000000L;
 
         const int MAX_EXPONENT = 0x7fe; // 2046
 
-        public static string Ieee754String(ulong v)
+        public static string DebugBinary(ulong v)
         {
             string res = Convert.ToString((long)v, 2);
             while (res.Length < 64)
             {
                 res = '0' + res;
             }
-            return res[0] + " " + res.Substring(1, 11) + " " + res.Substring(12);
+            for (int q = 4, position = 0; q <= 60; q += 4, position++)
+            {
+                res = res.Substring(0, q + position) + ' ' + res.Substring(q + position);
+            }
+            return res;
         }
         
         // Number format: d{f...}   Note: "d" must be 1-9 
-        internal static double Parse(bool sign, string digitAndOptionalFraction, int exponent)
+        internal static double Parse(bool sign, string digitAndOptionalFraction, int base10Exponent)
         {
             string ieeeString;
 
-            int base10index = exponent + Base2Lookup.EXPONENT_OFFSET;
+            int base10index = base10Exponent + Base2Lookup.EXPONENT_OFFSET;
+
+            // Sanity check
             if (base10index < 0)
             {
-                return 0;  // Too small to make any IEEE-754 bits
+                // Very small number, not worth bothering with. Return as 0
+                return 0;
             }
+
+            // Sanity check
             if (base10index >= Base2Lookup.Cache.Length)
             {
-                return Double.NaN;   // Overflow are dealt with as a Not a Number
+                // Huge number, outside of IEEE 754 double precision. Return as NaN
+                return Double.NaN;
             }
+
+            // Core base10 to base2 operation
             Base2Lookup base2Entry = Base2Lookup.Cache[base10index];
-            int exp2Update = 0;
             decimal base2 = Decimal.Parse(digitAndOptionalFraction.Substring(0, 1) + 
                                           '.' +
                                           digitAndOptionalFraction.Substring(1));
+
+            // The number in "base2" MUST after this be >= 1 and < 20
             base2 *= base2Entry.MantissaMultiplier;
+
+            // Entering the "bit fiddling" where we are stuffing an "ulong" with IEEE-754 data
+            int base2Exponent = 0;
+
+            // Use "ulong" but realize its limits...
             if (base2 > 17)
             {
-                base2 /= 2;  // Be kind to "unlong"
-                exp2Update++;
+                base2 /= 2;
+                base2Exponent++;
             }
-            ulong binary = (ulong)(base2 * LARGEST_SAFE_MULTIPLIER);
-            ieeeString = Ieee754String(binary);
-            while ((binary & SCALE_POINT) != 0)
+            ulong ieee754 = (ulong)(base2 * LARGEST_SAFE_MULTIPLIER);
+            ieeeString = DebugBinary(ieee754);
+            while ((ieee754 & SCALE_POINT_MASK) != 0)
             {
-                binary >>= 1;
-                ieeeString = Ieee754String(binary);
-                exp2Update++;
+                ieee754 >>= 1;
+                ieeeString = DebugBinary(ieee754);
+                base2Exponent++;
             }
-            exp2Update += base2Entry.Base2Exponent;
-            if (exp2Update > MAX_EXPONENT)
-            {
-                return Double.NaN;   // Overflow are dealt with as a Not a Number
-            }
-            if (exp2Update <= 0)  // Are we dealing with unormalized numbers?
-            {
-                if (exp2Update < -ES6NumberFormatter.MANTISSA_SIZE)
-                {
-                    return 0;  // Too small to make any IEEE-754 bits
-                }
-                binary += MANTISSA_ROUNDER << -exp2Update;
-                ieeeString = Ieee754String(binary);
-                binary >>= -exp2Update;
-                ieeeString = Ieee754String(binary);
 
-                exp2Update = 0;
+            // Add base10 to base2 exponent offset
+            base2Exponent += base2Entry.Base2Exponent;
+
+            // Setup. "Roundcontrol to major Tom"
+            ulong roundControl = SCALE_POINT_MASK;
+
+            // Normalized versus Unormalized
+            if (base2Exponent <= 0)
+            {
+                // Unormalized number. Ignore numbers that do not make any IEEE-754 bits 
+                if (base2Exponent < -ES6NumberFormatter.MANTISSA_SIZE)
+                {
+                    return 0; 
+                }
+                ieee754 += MANTISSA_ROUNDER << -base2Exponent;
+                ieeeString = DebugBinary(ieee754);
+                ieee754 >>= -base2Exponent;
+                ieeeString = DebugBinary(ieee754);
+
+                // Unormalized numbers have a zero exponent
+                base2Exponent = 0;
             }
             else
             {
-                binary <<= 1;  // Normalized numbers use a "virtual" 1.nnnnnn
-                binary &= ~SCALE_POINT;
-                ieeeString = Ieee754String(binary);
-                binary += MANTISSA_ROUNDER;
-                ieeeString = Ieee754String(binary);
+                // Normalized number. Assume a "virtual" 1 like 1.nnnnnn
+                ieee754 <<= 1;
+                ieeeString = DebugBinary(ieee754);
+                ieee754 += MANTISSA_ROUNDER;
+                ieeeString = DebugBinary(ieee754);
+                roundControl <<= 1;
             }
-            if ((binary & SCALE_POINT) != 0)
+
+            // There could be a carry from rounding
+            if ((ieee754 & roundControl) != 0)
             {
-                binary >>= 1;
+                ieee754 >>= 1;
+                ieeeString = DebugBinary(ieee754);
+                base2Exponent++;
             }
-            binary >>= 9;
-            ieeeString = Ieee754String(binary);
-            binary += (ulong)exp2Update << ES6NumberFormatter.MANTISSA_SIZE;
-            ieeeString = Ieee754String(binary);
+
+            // Check again for overflow
+            if (base2Exponent > MAX_EXPONENT)
+            {
+                return Double.NaN;   // Return as a Not a Number
+            }
+
+            // Remove possible "virtual" 1
+            ieee754 &= ~SCALE_POINT_MASK;
+            ieeeString = DebugBinary(ieee754);
+
+            // Mantissa MSB is now at bit 60. Move it down to its proper position at bit 51
+            ieee754 >>= 9;
+            ieeeString = DebugBinary(ieee754);
+
+            // Add exponent shifted into the proper position
+            ieee754 += (ulong)base2Exponent << ES6NumberFormatter.MANTISSA_SIZE;
+            ieeeString = DebugBinary(ieee754);
+
+            // Finally, add the optional sign bit
             if (sign)
             {
-                binary |= MSB_U64;
+                ieee754 |= MSB_U64;
             }
-            return BitConverter.Int64BitsToDouble((long)binary);
+
+            // Make it double please!
+            return BitConverter.Int64BitsToDouble((long)ieee754);
         }
 
         public static double TryParse(string number, out string error)
         {
+            error = null;
             if (!NUMBER_FORMAT.IsMatch(number))
             {
                 error = "Number syntax error";
                 return Double.NaN;
             }
-            error = null;
-            int startNum = 0;
+
+            // Find and remove possible sign
             bool signBit = false;
             if (number[0] == '-')
             {
                 signBit = true;
-                startNum = 1;
+                number = number.Substring(1);
             }
+
+            // Find and remove possible exponent
             int exponent = 0;
             int startExp = number.IndexOfAny(EXPONENT_LETTERS);
             if (startExp > 0)
@@ -146,28 +195,40 @@ namespace Org.Webpki.Es6Numbers
                 exponent = int.Parse(number.Substring(startExp + 1));
                 number = number.Substring(0, startExp);
             }
+
+            // Remove leading zeroes
             while (number.Length > 0 && number[0] == '0')
             {
                 number = number.Substring(1);
             }
+
+            // Remove trailing zeroes
             while (number.Length > 0 && number[number.Length - 1] == '0')
             {
                 number = number.Substring(0, number.Length - 1);
             }
+
+            // One or more zeroes only?
             if (number.Length == 0)
             {
                 return 0;
             }
+
+            // Find and remove possible decimal point
             int point = number.IndexOf('.');
             if (point >= 0)
             {
+                // It was just 0.0?
                 if (number.Length == 1)
                 {
                     return 0;
                 }
                 if (point == 0)
                 {
+                    // .{000}fff
                     number = number.Substring(1);
+
+                    // Normalize to d.ff
                     exponent--;
                     while (number[0] == '0')
                     {
@@ -177,21 +238,22 @@ namespace Org.Webpki.Es6Numbers
                 }
                 else
                 {
-                    if (point == number.Length - 1)
-                    {
-                        number = number.Substring(0, point);
-                    }
-                    else
-                    {
-                        number = number.Substring(0, point) + number.Substring(point + 1);
-                    }
+                    // ddd.{fff}
+                    number = number.Substring(0, point) + (point == number.Length - 1 ? "" : number.Substring(point + 1));
+
+                    // Normalize to d.dd{fff}
                     exponent -= point - 1;
                 }
             }
-            double d = Parse(signBit, number.Substring(startNum), exponent);
+            else
+            {
+                // Normalize ddd to d.ff
+                exponent += number.Length - 1;
+            }
+            double d = Parse(signBit, number, exponent);
             if (Double.IsNaN(d))
             {
-                error = "Number out of limits";
+                error = "Number out of range";
             }
             return d;
         }
