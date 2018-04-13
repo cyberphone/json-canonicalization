2e70dc975a5996ab6d0b2dfc43f0f75cc2b7a3b8
Anders Rundgren <anders.rundgren.net@gmail.com>
2018-04-13 21:06:56 +0200

    temp

--- dotnet/org.webpki.es6numbers/ES6NumberParser.cs
+++ dotnet/org.webpki.es6numbers/ES6NumberParser.cs
@@ -40,6 +40,19 @@ namespace Org.Webpki.Es6Numbers
 
         const int MAX_EXPONENT = 0x7fe; // 2046
 
+        const decimal LSB_FACTOR = 0.000000000000000011102230246m;
+
+        public static string ieeeString;
+
+        public static bool roundBit;
+
+        public static string higher;
+        public static string lower;
+        public static string orig;
+        public static bool upRound;
+        public static bool evenHigh;
+        public static bool evenLow;
+
         public static string DebugBinary(ulong v)
         {
             string res = Convert.ToString((long)v, 2);
@@ -53,11 +66,28 @@ namespace Org.Webpki.Es6Numbers
             }
             return res;
         }
+
+        static string truncate(decimal v)
+        {
+            string str = v.ToString();
+            if (str.Length > 22)
+            {
+                str = str.Substring(0, 22);
+            }
+            else
+            {
+                while (str.Length < 22)
+                {
+                    str += '0';
+                }
+            }
+            return str;
+        }
         
         // Number format: d{f...}   Note: "d" MUST be 1-9 
         internal static double Parse(bool sign, string digitAndOptionalFraction, int base10Exponent)
         {
-            string ieeeString;
+            
 
             int base10index = base10Exponent + Base2Lookup.EXPONENT_OFFSET;
 
@@ -77,12 +107,12 @@ namespace Org.Webpki.Es6Numbers
 
             // Core base10 to base2 operation
             Base2Lookup base2Entry = Base2Lookup.Cache[base10index];
-            decimal base2 = Decimal.Parse(digitAndOptionalFraction.Substring(0, 1) + 
-                                          '.' +
-                                          digitAndOptionalFraction.Substring(1));
+            decimal decimalValue= Decimal.Parse(digitAndOptionalFraction.Substring(0, 1) +
+                                                '.' +
+                                                digitAndOptionalFraction.Substring(1));
 
             // The number in "base2" MUST after this be >= 1 and < 20
-            base2 *= base2Entry.MantissaMultiplier;
+            decimal base2 = decimalValue * base2Entry.MantissaMultiplier;
 
             // Entering the "bit fiddling" where we are stuffing an "ulong" with IEEE-754 data
             int base2Exponent = 0;
@@ -102,6 +132,8 @@ namespace Org.Webpki.Es6Numbers
             ulong roundControl = SCALE_POINT_MASK;
 
             // Normalized versus Denormalized
+            string adjusted = "no";
+
             if (base2Exponent <= 0)
             {
                 // Denormalized number. Ignore numbers that do not make any IEEE-754 bits 
@@ -109,7 +141,6 @@ namespace Org.Webpki.Es6Numbers
                 {
                     return 0; 
                 }
-                ieee754 += MANTISSA_ROUNDER << -base2Exponent;
                 ieeeString = DebugBinary(ieee754);
                 ieee754 >>= -base2Exponent;
                 ieeeString = DebugBinary(ieee754);
@@ -122,10 +153,102 @@ namespace Org.Webpki.Es6Numbers
                 // Normalized number. Assume a "virtual" 1 like 1.nnnnnn
                 ieee754 <<= 1;
                 ieeeString = DebugBinary(ieee754);
-                ieee754 += MANTISSA_ROUNDER;
-                ieeeString = DebugBinary(ieee754);
-                roundControl <<= 1;
+                 roundControl <<= 1;
             }
+
+            orig = truncate(decimalValue);
+            roundBit = (ieee754 & MANTISSA_ROUNDER) != 0;
+            decimal lowerValue = decimalValue  - (decimalValue * (LSB_FACTOR * (ieee754 & ((MANTISSA_ROUNDER << 1) - 1)))) / (MANTISSA_ROUNDER << 1);
+            lower = truncate(lowerValue);
+            decimal higherValue = decimalValue + decimalValue * LSB_FACTOR;
+                                                 
+            higher = truncate(higherValue);
+            upRound = (higherValue - decimalValue) < (decimalValue - lowerValue);
+            evenHigh = true;
+            int q = 0;
+            foreach (char c in higher)
+            {
+                char d = orig[q++];
+                if (c != d)
+                {
+                    if ((c & 1) == 1 || (d & 1) == 0)
+                    {
+                        evenHigh = false;
+                    }
+                    break;
+                }
+            }
+            q = 0;
+            evenLow = true;
+            foreach (char c in lower)
+            {
+                char d = orig[q++];
+                if (c != d)
+                {
+                    if ((c & 1) == 1 || (d & 1) == 0)
+                    {
+                        evenLow = false;
+                    }
+                    break;
+                }
+            }
+            decimal j2 = decimal.Round(((decimal)(ieee754 >> 8) * Base10Lookup.Cache[base2Exponent].FractionMultiplier) / 0x0010000000000000, 22);
+            decimal j3 = decimal.Round(((decimal)((ieee754 >> 8) + 1) * Base10Lookup.Cache[base2Exponent].FractionMultiplier) / 0x0010000000000000, 22);
+            if (j3 > 10)
+            {
+                j3 /= 10;
+                j2 /= 10;
+            }
+            lower = truncate(j2);
+            higher = truncate(j3);
+            j3 -= decimalValue + decimalValue - j2;
+            evenHigh = j3 < 0;
+            upRound = j3 == 0;
+            //           if (j3 < 0)
+            if (j3 < 0 || (j3 == 0 && (ieee754 & 0xff) != 0 && (ieee754 & 0x100) != 0))
+            {
+                ieee754 += 0x100;
+            }
+            /*
+            if ((ieee754 & MANTISSA_ROUNDER) != 0)
+            {
+                if ((ieee754 & (MANTISSA_ROUNDER - 1)) != 0)
+                {
+                    Console.WriteLine("Round CORE: " + ieeeString + " o=" + digitAndOptionalFraction + " a=" + adjusted);
+                    ieee754 += MANTISSA_ROUNDER;
+                }
+                else
+                {
+                    adjusted = (decimalValue * 1.000000000000000055511151m).ToString();
+                    string refValue = digitAndOptionalFraction + "0000000000000000000000000";
+                    int q = 0;
+                    foreach (char c in adjusted)
+                    {
+                        if (c == '.')
+                        {
+                            continue;
+                        }
+                        if (c != refValue[q++])
+                        {
+                            if ((c & 1) == 0 || c < '2' || c > '8')
+                            {
+                                Console.WriteLine("Round CLOSEST: " + ieeeString + " o=" + refValue + " a=" + adjusted);
+                                ieee754 += MANTISSA_ROUNDER;
+                            }
+                            else
+                            {
+                                Console.WriteLine("NO Round: " + ieeeString + " o=" + refValue + " a=" + adjusted);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("NO Round: " + ieeeString + " o=" + digitAndOptionalFraction + " a=" + adjusted);
+            }
+            */
 
             // There could be a carry from rounding
             if ((ieee754 & roundControl) != 0)
@@ -143,15 +266,15 @@ namespace Org.Webpki.Es6Numbers
 
             // Remove possible "virtual" 1
             ieee754 &= ~SCALE_POINT_MASK;
-            ieeeString = DebugBinary(ieee754);
+ //           ieeeString = DebugBinary(ieee754);
 
             // Mantissa MSB is now at bit 59. Move it down to its proper position at bit 51
             ieee754 >>= 8;
-            ieeeString = DebugBinary(ieee754);
+ //           ieeeString = DebugBinary(ieee754);
 
             // Add exponent shifted into the proper position
             ieee754 += (ulong)base2Exponent << ES6NumberFormatter.MANTISSA_SIZE;
-            ieeeString = DebugBinary(ieee754);
+ //           ieeeString = DebugBinary(ieee754);
 
             // Finally, add the optional sign bit
             if (sign)
@@ -194,7 +317,7 @@ namespace Org.Webpki.Es6Numbers
             {
                 number = number.Substring(1);
             }
-
+            // 1.0228401905118014e+64
             // Find and remove possible decimal point
             int point = number.IndexOf('.');
             if (point == 0)
