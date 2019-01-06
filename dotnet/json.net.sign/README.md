d83001c5a287b97c473986195f3cd105f328b94a
Anders Rundgren <anders.rundgren.net@gmail.com>
2019-01-06 20:19:45 +0100

    Final part added to the .NET implementation

---
+++ dotnet/json.net.sign/README.md
@@ -0,0 +1,90 @@
+## Clear Text Signature Sample Solution
+
+The sample is based on Newtonsoft's Json.NET API (nowadays recommended by Microsoft).  
+
+The code is _deliberately_ simplistic
+with respect to the cryptographic part (using a hard-coded algorithm and key),
+while the rest is pretty universal.
+
+For creating "signable" JSON objects, developers only need adding a `SignatureObject` property
+with a JSON property name of their liking:
+```c#
+[JsonProperty("signature", Required = Required.Always)]
+public SignatureObject Signature { get; set; }
+```
+
+Expected printout from the sample program:
+```json
+{
+  "id": "johndoe",
+  "counter": 3,
+  "list": [
+    "yes",
+    "no"
+  ],
+  "€": true,
+  "signature": {
+    "alg": "HS256",
+    "kid": "mykey",
+    "val": "rlKLoCBExrwB7NaChPtQ3cAxr83eGdpLA7txrg49slw"
+  }
+}
+```
+
+## Signing JSON Arrays
+The system also provides support for signing arrays:
+```c#
+using System;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+
+using json.net.signaturesupport;
+
+namespace json.net.sign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an array instance
+            List<object> jsonArray = new List<object>();
+            jsonArray.Add(3);
+            jsonArray.Add("johndoe");
+            jsonArray.Add(true);
+            jsonArray.Add(new string[] { "yes", "no" });
+
+            // Sign array
+            Signature.Sign(jsonArray);
+
+            // Serialize array to JSON
+            String json = JsonConvert.SerializeObject(jsonArray, Formatting.Indented);
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(json);
+
+            // Recreate array using received JSON
+            List<object> receivedObject = JsonConvert.DeserializeObject<List<object>>(json);
+
+            // Verify signature
+            Console.WriteLine("Signature verified=" + (Signature.Verify(receivedObject) != null));
+        }
+    }
+}
+```
+Expected JSON printout from the sample above:
+```json
+[
+  3,
+  "johndoe",
+  true,
+  [
+    "yes",
+    "no"
+  ],
+  {
+    "alg": "HS256",
+    "kid": "mykey",
+    "val": "FXlIAzZ6UUIAOoTYRI84FOwcNFw9tDEPQEd0ZlBWix4"
+  }
+]
+```
---
+++ dotnet/json.net.sign/SignatureSupport.cs
@@ -0,0 +1,208 @@
+/*
+ *  Copyright 2006-2019 WebPKI.org (http://webpki.org).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+// Ultra-simple "Signed JSON" based on Canonicalization
+
+namespace json.net.signaturesupport
+{
+    public class Base64UrlConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var output = (string)reader.Value;
+            output = output.Replace('-', '+');
+            output = output.Replace('_', '/');
+            switch (output.Length % 4)
+            {
+                case 0: break;
+                case 2: output += "=="; break;
+                case 3: output += "="; break;
+                default: throw new System.ArgumentOutOfRangeException("input", "Illegal base64url string!");
+            }
+            return Convert.FromBase64String(output);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var output = Convert.ToBase64String((byte[])value);
+            output = output.Split('=')[0];
+            output = output.Replace('+', '-');
+            output = output.Replace('/', '_');
+            writer.WriteValue(output);
+        }
+    }
+
+    public class Canonicalizer : DefaultContractResolver
+    {
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            return base.CreateProperties(type, memberSerialization).OrderBy(p => p.PropertyName, StringComparer.Ordinal).ToList();
+        }
+    }
+
+    public class SignatureObject
+    {
+        [JsonProperty("alg", Required = Required.Always)]
+        public string Algorithm { get; internal set; }
+
+        [JsonProperty("kid", Required = Required.Always)]
+        public string KeyId { get; internal set; }
+
+        [JsonConverter(typeof(Base64UrlConverter))]
+        [JsonProperty("val", Required = Required.Always, NullValueHandling = NullValueHandling.Ignore)]
+        public byte[] SignatureValue { get; internal set; }
+    }
+
+    public static class Signature
+    {
+        const string ALGORITHM = "HS256";
+
+        const string KEY_ID = "mykey";
+
+        static byte[] SECRET_KEY = { 0xF4, 0xC7, 0x4F, 0x33,
+                                     0x98, 0xC4, 0x9C, 0xF4,
+                                     0x6D, 0x93, 0xEC, 0x98,
+                                     0x18, 0x83, 0x26, 0x61,
+                                     0xA4, 0x0B, 0xAE, 0x4D,
+                                     0x20, 0x4D, 0x75, 0x50,
+                                     0x36, 0x14, 0x10, 0x20,
+                                     0x74, 0x34, 0x69, 0x09 };
+
+        static byte[] HmacObject(byte[] data)
+        {
+            using (HMACSHA256 hmac = new HMACSHA256(SECRET_KEY))
+            {
+                using (MemoryStream inStream = new MemoryStream(data))
+                {
+                    return hmac.ComputeHash(inStream);
+                }
+            }
+        }
+
+        private static PropertyInfo GetSignatureProperty(object obj)
+        {
+            foreach (var property in obj.GetType().GetProperties(BindingFlags.DeclaredOnly |
+                                                                 BindingFlags.Public |
+                                                                 BindingFlags.Instance))
+            {
+                if (property.PropertyType == typeof(SignatureObject))
+                {
+                    return property;
+                }
+            }
+            throw new MemberAccessException("Property \"SignatureObject\" missing in: " + obj.GetType().ToString());
+        }
+
+        private static byte[] CanonicalizeObject(object obj)
+        {
+            return new UTF8Encoding(false).GetBytes(
+                JsonConvert.SerializeObject(obj,
+                                            Formatting.None,
+                                            new JsonSerializerSettings { ContractResolver = new Canonicalizer() }));
+        }
+
+        public static SignatureObject Sign(object obj)
+        {
+            // Create and initialize an empty signature object
+            SignatureObject signatureObject = new SignatureObject
+            {
+                Algorithm = ALGORITHM,
+                KeyId = KEY_ID
+            };
+
+            if (obj is List<object>)
+            {
+                // We are signing an array, append signature
+                ((List<object>)obj).Add(signatureObject);
+            }
+            else
+            {
+                // We are signing an object, assign signature to it
+                GetSignatureProperty(obj).SetValue(obj, signatureObject);
+            }
+
+            // Canonicalize the completed object
+            byte[] canonicalizedUtf8 = CanonicalizeObject(obj);
+
+            // Finally add the signature value to the signature object
+            signatureObject.SignatureValue = HmacObject(canonicalizedUtf8);
+            return signatureObject;
+        }
+
+        public static SignatureObject Verify(object obj)
+        {
+            SignatureObject signatureObject;
+            if (obj is List<object>)
+            {
+                // We are verifying a signed array, fetch the last element
+                JObject jobject = (JObject)((List<object>)obj).Last();
+
+                // Since the deserializer does not know what a SignatureObject is,
+                // it returs a generic object which we remap to a SignatureObject.
+                signatureObject = jobject.ToObject<SignatureObject>();
+
+                // Finally, the last element is replaced by the true SignatureObject.
+                ((List<object>)obj).Remove(jobject);
+                ((List<object>)obj).Add(signatureObject);
+            }
+            else
+            {
+                // We are verifying a signed object, get the signature object
+                signatureObject = (SignatureObject)GetSignatureProperty(obj).GetValue(obj);
+            }
+
+            // Verify correctness of container
+            if (!signatureObject.Algorithm.Equals(ALGORITHM) || !signatureObject.KeyId.Equals(KEY_ID))
+            {
+                throw new CryptographicException("Unexpected \"SignatureObject\" arguments: " +
+                    JsonConvert.SerializeObject(signatureObject));
+            }
+
+            // Fetch signature value
+            byte[] signatureValue = signatureObject.SignatureValue;
+
+            // Hide signature value from the serializer
+            signatureObject.SignatureValue = null;
+
+            // Canonicalize the object - signature value
+            byte[] canonicalizedUtf8 = CanonicalizeObject(obj);
+
+            // Restore signature object
+            signatureObject.SignatureValue = signatureValue;
+
+            // Verify signature value
+            return signatureValue.SequenceEqual(HmacObject(canonicalizedUtf8)) ? signatureObject : null;
+        }
+    }
+}
